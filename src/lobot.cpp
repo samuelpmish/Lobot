a2d0cbe46d12c89fc6eb068e1e112f14b5d61de9
gyfis <gyfis@seznam.cz>
2018-05-23 17:30:54 +0200

    Specified one file for the pybind11 module creation

--- src/lobot.cpp
+++ src/lobot.cpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "bot.cpp"
 #include "Ball.h"
+
 #include <pybind11/pybind11.h>
 
 PYBIND11_MODULE(Lobot, m) {
@@ -10,4 +12,8 @@ PYBIND11_MODULE(Lobot, m) {
 		.def_readwrite("velocity", &Ball::v)
 		.def_readwrite("angular_velocity", &Ball::w)
 		.def("step", &Ball::step);
+	
+	pybind11::class_<Bot>(m, "Bot")
+		.def(pybind11::init<int>())
+		.def("get_response", &Bot::get_response);
 }
