b862c7dc135276dc4e750361696a95826811e4c0
samuelpmish <samuelpmish@users.noreply.github.com>
2018-05-23 14:30:05 -0700

    tabs to spaces

--- src/lobot.cpp
+++ src/lobot.cpp
@@ -7,19 +7,19 @@
 #include <pybind11/pybind11.h>
 
 PYBIND11_MODULE(Lobot, m) {
-	pybind11::class_<Ball>(m, "Ball")
-		.def(pybind11::init<>())
-		.def_readwrite("position", &Ball::x)
-		.def_readwrite("velocity", &Ball::v)
-		.def_readwrite("angular_velocity", &Ball::w)
-		.def("step", &Ball::step);
-	
-	pybind11::class_<Bot>(m, "Bot")
-		.def(pybind11::init<int>())
-		.def("get_response", &Bot::get_response);
+  pybind11::class_<Ball>(m, "Ball")
+    .def(pybind11::init<>())
+    .def_readwrite("position", &Ball::x)
+    .def_readwrite("velocity", &Ball::v)
+    .def_readwrite("angular_velocity", &Ball::w)
+    .def("step", &Ball::step);
+  
+  pybind11::class_<Bot>(m, "Bot")
+    .def(pybind11::init<int>())
+    .def("get_response", &Bot::get_response);
 
-	pybind11::class_<vec3>(m, "vec3")
-		.def(pybind11::init<float, float, float>())
+  pybind11::class_<vec3>(m, "vec3")
+    .def(pybind11::init<float, float, float>())
     .def("__getitem__", [](const vec3 & v, size_t i){ return v[i]; })
     .def("__setitem__", [](vec3 & v, size_t i, float f){ v[i] = f; });
 }
